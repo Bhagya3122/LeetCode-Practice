6b0fd644a24ea558a444af7290b75f8c6cf1bd5d
Bhagya3122 <104565704+Bhagya3122@users.noreply.github.com>
2026-03-03 16:52:52 +0530

    Create Palindrome.py
    Function to check a Palindrome

---
+++ Palindrome.py
@@ -0,0 +1,25 @@
+'''Given an integer x, return true if x is a palindrome, and false otherwise.
+Example 1:
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.'''
+
+y=input("Enter the number or text you want to check")
+def isPalindrome(x):
+    y= str(x)
+    for i in range(0,len(y)):
+            if y[i]!= y[len(y)-1-i]:
+                return False
+    else:
+        return True
+
+check= isPalindrome(y)
+print(check)
